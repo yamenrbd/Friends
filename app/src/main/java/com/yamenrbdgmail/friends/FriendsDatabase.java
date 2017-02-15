1828377e5d62f486d6cddc9c9448f3b9047320c1
yamen_rbd <yamenrbd@gmail.com>
2017-02-15 18:58:22 +0300

    3

--- app/src/main/java/com/yamenrbdgmail/friends/FriendsDatabase.java
+++ app/src/main/java/com/yamenrbdgmail/friends/FriendsDatabase.java
@@ -1,7 +1,9 @@
 package com.yamenrbdgmail.friends;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by German Center on 15/02/2017.
@@ -20,5 +22,19 @@ public class FriendsDatabase extends SQLiteOpenHelper {
     public FriendsDatabase(Context context){
         super(context ,DATABASE_NAME,null,DATABASE_VERSION);
         mContext = context ;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+ Tables.FRIENDS "("
+                + BaseColumns._ID+ " INTEGER PRIMARY KEY AUTOINCREMINT,"
+                + FriendsContract.FriendsColumns.FRIENDS_NAME+" TEXT NOT NULL ,"
+                + FriendsContract.FriendsColumns.FRIENDS_EMAIL+" TEXT NOT NULL,"
+                + FriendsContract.FriendsColumns.FRIENDS_PHONE+" TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
