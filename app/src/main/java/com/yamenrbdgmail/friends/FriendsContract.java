6639c87929389721a07d35e78a31a7aef12b6aa9
yamen_rbd <yamenrbd@gmail.com>
2017-02-15 18:09:39 +0300

    2

--- app/src/main/java/com/yamenrbdgmail/friends/FriendsContract.java
+++ app/src/main/java/com/yamenrbdgmail/friends/FriendsContract.java
@@ -1,8 +1,37 @@
 package com.yamenrbdgmail.friends;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 /**
  * Created by German Center on 15/02/2017.
  */
 
 public class FriendsContract {
+    interface FriendsColumns{
+        String FRIENDS_NAME = "friends_name";
+        String FRIENDS_EMAIL = "friends_email";
+        String FRIENDS_PHONE = "friends_phone";
+    }
+    public static final String CONTENT_AUTHORITY ="org.yamenrbdgmail.android.friends.provider";
+    public static final Uri BASE_CONTENT_URI =Uri.parse("content://" +CONTENT_AUTHORITY);
+    public static final String PATH_FRIENDS ="friends";
+
+    private static final String [] TOP_LEVEL_PATH={
+            PATH_FRIENDS
+    };
+
+    public static class Friends implements FriendsColumns , BaseColumns{
+        public static final Uri CONTENT_URI=
+                BASE_CONTENT_URI.buildUpon().appendEncodedPath(PATH_FRIENDS).build();
+        public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd."+CONTENT_AUTHORITY+".friends";
+        public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd."+CONTENT_AUTHORITY+".friends";
+
+        public static Uri buildFriendUri (String friendId){
+            return CONTENT_URI.buildUpon().appendEncodedPath(friendId).build();
+        }
+        public static String getFriendId(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+    }
 }
--- app/src/main/java/com/yamenrbdgmail/friends/FriendsDatabase.java
+++ app/src/main/java/com/yamenrbdgmail/friends/FriendsDatabase.java
@@ -1,8 +1,24 @@
 package com.yamenrbdgmail.friends;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by German Center on 15/02/2017.
  */
 
-public class FriendsDatabase {
+public class FriendsDatabase extends SQLiteOpenHelper {
+    private static final String TAG = FriendsDatabase.class.getSimpleName();
+    private static final String DATABASE_NAME = "friends.db";
+    private static final int DATABASE_VERSION = 2;
+    private Context mContext;
+
+    interface Tables {
+        String FRIENDS ="friends";
+    }
+
+    public FriendsDatabase(Context context){
+        super(context ,DATABASE_NAME,null,DATABASE_VERSION);
+        mContext = context ;
+    }
 }
